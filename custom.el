dab14f99867b03ec1daedd6daf86578c0a73a751
Zekun Shi <emperorview@gmail.com>
2021-11-24 14:32:28 +0800

    update

--- custom.el
+++ custom.el
@@ -3,10 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(dired-narrow))
  '(send-mail-function 'mailclient-send-it))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(vterm-color-black ((t (:background "#839496")))))
--- packages.el
+++ packages.el
@@ -7,47 +7,47 @@
 
 
 ;; to install some-package from melpa, elpa or emacsmirror:
-;(package! some-package)
+                                        ;(package! some-package)
 
 ;; to install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. you'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
-;(package! another-package
-;  :recipe (:host github :repo "username/repo"))
+                                        ;(package! another-package
+                                        ;  :recipe (:host github :repo "username/repo"))
 
 ;; if the package you are trying to install does not contain a packagename.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
-;(package! this-package
-;  :recipe (:host github :repo "username/repo"
-;           :files ("some-file.el" "src/lisp/*.el")))
+                                        ;(package! this-package
+                                        ;  :recipe (:host github :repo "username/repo"
+                                        ;           :files ("some-file.el" "src/lisp/*.el")))
 
 ;; if you'd like to disable a package included with doom, you can do so here
 ;; with the `:disable' property:
-;(package! builtin-package :disable t)
+                                        ;(package! builtin-package :disable t)
 
 ;; you can override the recipe of a built in package without having to specify
 ;; all the properties for `:recipe'. these will inherit the rest of its recipe
 ;; from doom or melpa/elpa/emacsmirror:
-;(package! builtin-package :recipe (:nonrecursive t))
-;(package! builtin-package-2 :recipe (:repo "myfork/package"))
+                                        ;(package! builtin-package :recipe (:nonrecursive t))
+                                        ;(package! builtin-package-2 :recipe (:repo "myfork/package"))
 
 ;; specify a `:branch' to install a package from a particular branch or tag.
 ;; this is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
-;(package! builtin-package :recipe (:branch "develop"))
+                                        ;(package! builtin-package :recipe (:branch "develop"))
 
 ;; use `:pin' to specify a particular commit to install.
-;(package! builtin-package :pin "1a2b3c4d5e")
+                                        ;(package! builtin-package :pin "1a2b3c4d5e")
 
 
 ;; doom's packages are pinned to a specific commit and updated from release to
 ;; release. the `unpin!' macro allows you to unpin single packages...
-;(unpin! pinned-package)
+                                        ;(unpin! pinned-package)
 ;; ...or multiple packages
-;(unpin! pinned-package another-pinned-package)
+                                        ;(unpin! pinned-package another-pinned-package)
 ;; ...or *all* packages (not recommended; will likely break things)
-;(unpin! t)
+                                        ;(unpin! t)
 
 (package! nov)                          ;; for epub layer
 (package! auto-highlight-symbol)        ;; for spacemacs-navigation
@@ -66,7 +66,7 @@
 (package! shell-pop)
 (package! dired-toggle-sudo)
 (package! csv-mode)
-                                        ;; (package! org-download)
+;; (package! org-download)
 (package! systemd)
 (package! info-colors)
 (use-package! evil-tex
@@ -100,3 +100,4 @@
   :recipe (:host github :repo "magit/git-modes" :files ("gitconfig-mode.el")))
 (package! gitignore-mode
   :recipe (:host github :repo "magit/git-modes" :files ("gitignore-mode.el")))
+(package! dired-narrow)
